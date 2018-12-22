86450ea25663554923e9d865374db5b16bdc85bc
Mark Wassell <mpwassell@gmail.com>
2018-12-22 14:19:52 +0000

    Updates to crowbar pp

--- src/crowbar.ml
+++ src/crowbar.ml
@@ -3,6 +3,9 @@
 
   [1] https://github.com/stedolan/crowbar
 
+  TODO - For metavars with Ocaml types specified; can we pp a generic gen function?
+    Go through special cases in sail.ott and see if we should/can extend cgen to handle them
+    Add documentation
  *)
 open Types
 
@@ -16,6 +19,7 @@ let pp_cgen_preamble fd sd =
       else
         ()) sd.xd_embed
 
+let cgen_hom p = Auxl.hom_spec_for_hom_name "cgen" p.prod_homs
             
 let rec pp_element_list es = List.concat (List.map pp_element  es)
 
@@ -37,25 +41,28 @@ and pp_list_form (elb : element_list_body) : (string*string) list =
     | _ -> ""
   in
   [ ("list ( " ^ (mk_pairs elb.elb_es) ^ ")" , "xx") ]
-            
-let pp_prod_gen fd p =
-  
-  let ntmvs = pp_element_list p.prod_es in  
-  output_string fd "   map [ ";
 
-  (* If element is a list then want to emit "list gen" where gen is the appropriate gen
-     but if we have a pair or n-tuple then want something more - just might be better to generate new gen for each list form
-     So first walk over all rules/prods looking for list forms and subforms and build the list 
-     of list gens - what if recursive with current gen being defined ??
-  *)
-  output_string fd (String.concat "; " (List.map fst ntmvs));
-  output_string fd "] (fun ";
-  output_string fd (String.concat " "  (List.map snd ntmvs));
-  output_string fd  (" -> " ^ p.prod_name ^ " ( ");
-  output_string fd (String.concat ", " (List.map snd ntmvs));
-  output_string fd "));\n"
-                                                    
-  
+let pp_ctor p = String.capitalize_ascii (p.prod_name)
+            
+let pp_prod_gen_aux fd p =   match (pp_element_list p.prod_es) with
+  | [] -> output_string fd ("   const " ^ (pp_ctor p) ^ ";\n" )
+  | ntmvs ->
+     output_string fd "   map [ ";
+     output_string fd (String.concat "; " (List.map fst ntmvs));
+     output_string fd "] (fun ";
+     output_string fd (String.concat " "  (List.map snd ntmvs));
+     output_string fd  (" -> " ^ (pp_ctor p) ^ " ( ");
+     output_string fd (String.concat ", " (List.map snd ntmvs));
+     output_string fd "));\n"
+
+let pp_prod_gen fd p = match (cgen_hom p) with
+  | None -> pp_prod_gen_aux fd p
+  | Some hs ->  (List.iter (fun h -> match h with
+                                                           | Hom_string s -> output_string fd s
+                                                           | _ -> () ) hs);
+            output_string fd "\n"
+                   
+                   
 let pp_rule_gen fd fun_prefix r =
   let fname = (r.rule_ntr_name) ^ "_gen" in
   output_string fd ( fun_prefix  ^ fname ^ " = lazy (choose [ \n");
@@ -63,12 +70,10 @@ let pp_rule_gen fd fun_prefix r =
   output_string fd "])\n"
                 
                 
-let pp_cgen_generators fd sd = List.iter
-             (fun (pr,r) -> if r.rule_judgement || r.rule_semi_meta || r.rule_meta || r.rule_phantom then
-                         ()
-                       else
-                         pp_rule_gen fd pr r)
-             (List.combine ("let rec " :: (List.init (List.length sd.xd_rs - 1) (fun _ -> " and "))) sd.xd_rs)
+let pp_cgen_generators fd sd =
+  let rlist = List.filter (fun r -> not (r.rule_judgement || r.rule_semi_meta || r.rule_meta || r.rule_phantom)) sd.xd_rs in 
+  List.iter  (fun (pr,r) ->  pp_rule_gen fd pr r)
+             (List.combine ("let rec " :: (List.init (List.length rlist - 1) (fun _ -> " and "))) rlist)
 
 let pp_cgen_lazy_pat fd sd = List.iter
              (fun r -> if r.rule_judgement || r.rule_semi_meta || r.rule_meta || r.rule_phantom then
